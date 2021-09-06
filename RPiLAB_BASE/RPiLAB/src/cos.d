bcc8a7c2f7d470aebc3daeee8019f6c7e0041b2a
Bartosz-W <222146@edu.p.lodz.pl>
2021-09-06 23:30:41 +0200

    Probujemy cos wyswietlic na ekranie V1.03

--- RPiLAB_BASE/RPiLAB/src/cos.d
+++ RPiLAB_BASE/RPiLAB/src/cos.d
@@ -1,1 +1,112 @@
-src/cos.o: ../src/cos.cpp
+src/cos.o: ../src/cos.cpp ../src/main.h ../src/settings.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/memory.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/pagetable.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/armv6mmu.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/macros.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/types.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/screen.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcmframebuffer.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcmmailbox.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcm2835.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/sysconfig.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/chargenerator.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/timer.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/interrupt.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcm2835int.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exceptionstub.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/string.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/stdarg.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/logger.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exceptionhandler.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exception.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/startup.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspi.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspios.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/synchronize.h \
+ C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/memio.h \
+ ../src/utilities.h ../src/peripherals.h ../src/moje_klasy.h \
+ ../src/bitmapaZad0.h ../src/bitmapaZad30.h ../src/bitmapaZad60.h \
+ ../src/grzejnik_old_color.h ../src/grzejnik_color.h \
+ ../src/termometr_color.h ../src/wiatrak3.h ../src/wiatrak2.h \
+ ../src/wiatrak1.h
+
+../src/main.h:
+
+../src/settings.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/memory.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/pagetable.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/armv6mmu.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/macros.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/types.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/screen.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcmframebuffer.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcmmailbox.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcm2835.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/sysconfig.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/chargenerator.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/timer.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/interrupt.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/bcm2835int.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exceptionstub.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/string.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/stdarg.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/logger.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exceptionhandler.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/exception.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/startup.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspi.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspios.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/synchronize.h:
+
+C:\Users\wegne\Desktop\Studia\OSM-laby\gierka\RPiLAB_BASE\Library\include/uspienv/memio.h:
+
+../src/utilities.h:
+
+../src/peripherals.h:
+
+../src/moje_klasy.h:
+
+../src/bitmapaZad0.h:
+
+../src/bitmapaZad30.h:
+
+../src/bitmapaZad60.h:
+
+../src/grzejnik_old_color.h:
+
+../src/grzejnik_color.h:
+
+../src/termometr_color.h:
+
+../src/wiatrak3.h:
+
+../src/wiatrak2.h:
+
+../src/wiatrak1.h:
